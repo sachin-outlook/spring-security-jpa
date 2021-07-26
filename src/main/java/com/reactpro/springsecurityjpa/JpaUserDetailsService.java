70ba85754b3d3ee37709256bac713ed3dc6a08f7
Sachin <sachinalok@outlook.com>
2021-07-26 12:33:54 +0530

    All Changes

--- src/main/java/com/reactpro/springsecurityjpa/JpaUserDetailsService.java
+++ src/main/java/com/reactpro/springsecurityjpa/JpaUserDetailsService.java
@@ -1,14 +1,15 @@
 package com.reactpro.springsecurityjpa;
 
-import java.util.Optional;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.reactpro.springsecurityjpa.models.User;
 import com.reactpro.springsecurityjpa.repository.UserRepository;
 
 @Service
@@ -19,9 +20,17 @@ public class JpaUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
 		
-		Optional<User> user =  userRepository.findByUsername(arg0);
-		user.orElseThrow(()-> new UsernameNotFoundException("Not Found") );
-		return user.map(MyUserDetails::new).get();
+		org.springframework.security.core.userdetails.User user = new 
+				org.springframework.security.core.userdetails.User
+				("sachin", "sachin", true, true, true, true, 
+						Arrays.asList("ROLE_USER").stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())
+						);
+		
+		return user;
+		
+//		Optional<User> user =  userRepository.findByUsername(arg0);
+//		user.orElseThrow(()-> new UsernameNotFoundException("Not Found") );
+//		return user.map(MyUserDetails::new).get();
 		
 	}
 
--- src/main/java/com/reactpro/springsecurityjpa/controllers/UsersController.java
+++ src/main/java/com/reactpro/springsecurityjpa/controllers/UsersController.java
@@ -1,24 +1,32 @@
 package com.reactpro.springsecurityjpa.controllers;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersController {
 
+	Logger logger = LoggerFactory.getLogger(UsersController.class);
 	
 	@GetMapping("/")
-	public String welcome(){
-		return "<h1>Welcome</h1>";
+	public String welcome(Principal principal){
+		logger.error("Error");
+		return "<h1>Welcome</h1>" + principal.getName();
 	}
 	
 	@GetMapping("/api/users")
-	public String getUsers(){
-		return "<h1>Welcome Users</h1>";
+	public String getUsers(Principal principal){
+		return "<h1>Welcome Users</h1>" + principal.getName();
 	}
 	
 	@GetMapping("/api/admins")
-	public String getAdmins(){
-		return "<h1>Welcome Admin</h1>";
+	public String getAdmins(Principal principal){
+		return "<h1>Welcome Admin</h1>" + principal.getName();
 	}
+	
+	
 }
